9ba1537faec9c92c4ed1554a40c784c0b8336fee
Earl Pappi <94018014+EarlPappi@users.noreply.github.com>
2022-03-09 22:21:56 +0100

    Delete the-legend-of-mifi directory

--- the-legend-of-mifi/index.js
+++
@@ -1,6 +0,0 @@
-var menu = document.querySelector('#mobile-menu');
-var mainMenu = document.querySelector('#main-menu');
-
-menu.addEventListener('click', function(e){
-    mainMenu.classList.toggle('hide-menu')
-})
